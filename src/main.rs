a9b11cb5b3a67b5ac041ae1dd2ddd88d3766aaf9
Florentin Portets <florentin.portets@ynov.com>
2023-10-03 16:22:34 +0200

    2.0

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
+use actix_web::{App, HttpResponse, HttpServer, web, Result};
+use serde::Serialize;
+use env_logger::Env;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
+#[derive(Serialize)]
+struct RequestHeaders {
+    headers: Vec<(String, String)>,
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+async fn ping(request: actix_web::HttpRequest) -> HttpResponse {
+    let headers = request.headers();
+    let headers_vec: Vec<(String, String)> = headers
+        .iter()
+        .map(|(name, value)| (name.as_str().to_string(), value.to_str().unwrap_or("").to_string()))
+        .collect();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    let response_data = RequestHeaders { headers: headers_vec };
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+    HttpResponse::Ok().json(response_data)
+}
 
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+async fn not_found() -> HttpResponse {
+    HttpResponse::NotFound().finish()
+}
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let listen_port = std::env::var("PING_LISTEN_PORT")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(7878);
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/ping", web::get().to(ping))
+            .default_service(web::route().to(not_found))
+    })
+    .bind(("127.0.0.1", listen_port))?
+    .run()
+    .await
 }
